f5393db398c8db83df4c934b6d8c47fbc4d10d6c
ndasanbiagio <ndasanbiagio@gmail.com>
2024-01-18 15:17:50 -0300

    dinamicsMonth

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+
+def index(request):
+    return HttpResponse("This works!")
+
+def february(request):
+    return HttpResponse("Is February")
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Is January"
+    elif month == 'february':
+        challenge_text = 'Is February'
+    elif month == 'march':
+        challenge_text = 'Is March'
+    elif month == 'april':
+        challenge_text = 'Is April'
+    else:
+       return HttpResponseNotFound("This month is not supported!")      
+    return HttpResponse(challenge_text)
