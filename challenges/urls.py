f1e4c47a81435303955617da372f090d0b2ee9b1
ndasanbiagio <ndasanbiagio@gmail.com>
2024-01-18 16:02:09 -0300

    MoredinamicsMonth

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Asi para cuando no tiene dinamismo
     # path("january", views.index),
     # path("february", views.february),
-    path("<month>", views.monthly_challenges)
+    
+    #Con dinamismo
+    path("<int:month>", views.monthly_challenges_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -4,23 +4,57 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
+# --------- se iniciaria asi_ ->
 
-def index(request):
-    return HttpResponse("This works!")
+# def index(request):
+#     return HttpResponse("This works!")
 
-def february(request):
-    return HttpResponse("Is February")
+# def february(request):
+#     return HttpResponse("Is February")
 
-def monthly_challenges(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "Is January"
-    elif month == 'february':
-        challenge_text = 'Is February'
-    elif month == 'march':
-        challenge_text = 'Is March'
-    elif month == 'april':
-        challenge_text = 'Is April'
-    else:
-       return HttpResponseNotFound("This month is not supported!")      
+
+# --------- Se avanza con el curso, seria otra opcion para cargar.. pero serian 12 una para cada mes
+
+# def monthly_challenge(request, month):
+#     # challenge_text = None
+#     # if month == 'january':
+#     #     challenge_text = "Is January"
+#     # elif month == 'february':
+#     #     challenge_text = 'Is February'
+#     # elif month == 'march':
+#     #     challenge_text = 'Is March'
+#     # elif month == 'april':
+#     #     challenge_text = 'Is April'
+#     # else:
+#     #    return HttpResponseNotFound("This month is not supported!")      
+#     challenge_text = monthly_challenges[month]
+#     return HttpResponse(challenge_text)
+
+
+
+# Mejorando el dinamismo - con un diccionario
+monthly_challenges = {
+    "january": "Is January",
+    "february": "Is February",
+    "march": "Is March",
+    "april": "Is April",
+    "may": "Is May",
+    "june": "Is June",
+    "july": "Is July",
+    "august": "Is August",
+    "september": "Is September",
+    "october": "Is October",
+    "november": "Is November",
+    "december": "Is December"
+}
+
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This month is not supported!")
     return HttpResponse(challenge_text)
